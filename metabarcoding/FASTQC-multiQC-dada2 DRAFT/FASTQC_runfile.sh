e433da0d5570a7d939d5e68fc0ae02c14403851b
Kylie Langlois PhD <31413115+kylielanglois@users.noreply.github.com>
2023-08-17 13:05:14 -0700

    Add files via upload

--- metabarcoding/FASTQC-multiQC-dada2 DRAFT/FASTQC_runfile.sh
+++ metabarcoding/FASTQC-multiQC-dada2 DRAFT/FASTQC_runfile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #MUST DO BEFORE:
-#conda create -n multiqc -c bioconda -c conda-forge fastqc multiqc
-#conda activate multiqc
+#conda create -n multiqc_env -c bioconda -c conda-forge fastqc multiqc cutadapt
+#conda activate multiqc_env
 #export LC_ALL-en_US.UTF-8
 #export LANG=en_US.UTF-8
 
@@ -11,11 +11,21 @@ workingPATH="/Users/kylielanglois/Desktop/FASTQC_attempt_070323/fastq_multiqc_da
 fastq_directory=$workingPATH/"06152023_CO1_fastq"	# Name of the folder containing the fastqs. It must be located in the parent directory.
 scriptsPATH=$workingPATH/"FASTQC_MULTIQC_scripts_2023"
 
+#cutadapt--------
+cd $fastq_directory
+for f in *R1_001.fastq.gz; do
+
+    n=`echo $f | rev | cut -d _ -f 3- | rev` &&
+
+    cutadapt -j 8 -g GGDACWGGWTGAACWGTWTAYCCHCC -G CAAACAAATARDGGTATTCGDTY -o $n'_R1_001_trimmed.fastq.gz' -p $n'_R2_001_trimmed.fastq.gz' $f $n'_R2_001.fastq.gz'
+
+done
+
 #read 1 fastqc, multiqc-------------------
 cd $fastq_directory
 
 mkdir fastqc-R1
-fastqc *_R1_001.fastq.gz -o fastqc-R1
+fastqc *_R1_001_trimmed.fastq.gz -o fastqc-R1
 cd fastqc-R1
 multiqc --export .
 FASTQC_VAR1_80=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --input $fastq_directory/"fastqc-R1/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.8)
@@ -26,7 +36,7 @@ FASTQC_VAR1_95=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --inp
 #read 2 fastqc, multiqc-------------------
 cd ..
 mkdir fastqc-R2
-fastqc *_R2_001.fastq.gz -o fastqc-R2
+fastqc *_R2_001_trimmed.fastq.gz -o fastqc-R2
 cd fastqc-R2
 multiqc --export .
 FASTQC_VAR2_80=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --input $fastq_directory/"fastqc-R2/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.8)
@@ -36,24 +46,24 @@ FASTQC_VAR2_95=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --inpu
 
 #run dada2 filterandtrim-------------------
 cd $scriptsPATH
-Rscript --vanilla dada2_filteronly_runfile.r ${fastq_directory} ${FASTQC_VAR1_80} ${FASTQC_VAR2_80}
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_80} ${FASTQC_VAR2_80}
 DADA2_VAR_80=$(sed '2q;d' $fastq_directory/average_quality.txt)
 rm $fastq_directory/filtered/*_filt.fastq.gz
 rmdir $fastq_directory/filtered
 
-Rscript --vanilla dada2_filteronly_runfile.r ${fastq_directory} ${FASTQC_VAR1_85} ${FASTQC_VAR2_85}
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_85} ${FASTQC_VAR2_85}
 DADA2_VAR_85=$(sed '2q;d' $fastq_directory/average_quality.txt)
 rm $fastq_directory/filtered/*_filt.fastq.gz
 rmdir $fastq_directory/filtered
 
 
-Rscript --vanilla dada2_filteronly_runfile.r ${fastq_directory} ${FASTQC_VAR1_90} ${FASTQC_VAR2_90}
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_90} ${FASTQC_VAR2_90}
 DADA2_VAR_90=$(sed '2q;d' $fastq_directory/average_quality.txt)
 rm $fastq_directory/filtered/*_filt.fastq.gz
 rmdir $fastq_directory/filtered
 
 
-Rscript --vanilla dada2_filteronly_runfile.r ${fastq_directory} ${FASTQC_VAR1_95} ${FASTQC_VAR2_95}
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_95} ${FASTQC_VAR2_95}
 DADA2_VAR_95=$(sed '2q;d' $fastq_directory/average_quality.txt)
 rm $fastq_directory/filtered/*_filt.fastq.gz
 rmdir $fastq_directory/filtered
