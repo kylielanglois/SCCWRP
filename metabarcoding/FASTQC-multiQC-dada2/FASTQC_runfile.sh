4387ef9d823fc91a2ae740fde497e108699612e1
Kylie Langlois PhD <31413115+kylielanglois@users.noreply.github.com>
2023-09-12 16:16:00 -0700

    Update FASTQC_runfile.sh
    truncLen optimization

--- metabarcoding/FASTQC-multiQC-dada2/FASTQC_runfile.sh
+++ metabarcoding/FASTQC-multiQC-dada2/FASTQC_runfile.sh
@@ -2,14 +2,17 @@
 
 #MUST DO BEFORE:
 #conda create -n multiqc_env -c bioconda -c conda-forge fastqc multiqc cutadapt
-#conda activate multiqc
+#conda activate multiqc_env
 #export LC_ALL=en_US.UTF-8
 #export LANG=en_US.UTF-8
 
-project_shortname="rcbL_082523"
-workingPATH="/home/genomics/raw/rcbL_082523" 	# Path to the parent directory. It is not the directory where are stored the fastqs. Should NOT finish with "/"
-fastq_directory=$workingPATH/"rcbL_082523_demux"	# Name of the folder containing the fastqs. It must be located in the parent directory.
-scriptsPATH="/home/genomics/scripts/cutadapt-FASTQC-multiQC-dada2_scripts_2023"
+#user input-------------------
+project_shortname="" #PUT PROJECT SHORT NAME HERE
+workingPATH="/" 	# Path to the parent directory. It is not the directory where are stored the fastqs. Should NOT finish with "/"
+fastq_directory=$workingPATH/""	# Name of the folder containing the fastqs. It must be located in the parent directory. Should NOT finish with "/"
+scriptsPATH="" #where scripts are located; must contain fastqc_per_base_sequence_quality_dropoff.py dada2_runfile.r
+primerF="GTGYCAGCMGCCGCGGTAA" #forward primer
+primerR="CCGYCAATTYMTTTRAGTTT" #reverse primer
 
 #cutadapt--------
 cd $fastq_directory
@@ -17,7 +20,7 @@ for f in *R1_001.fastq.gz; do
 
     n=`echo $f | rev | cut -d _ -f 3- | rev` &&
 
-    cutadapt -j 8 -g AGGTGAAGTAAAAGGTTCWTACTTAAA -G CCTTCTAATTTACCWACWACTG -o $n'_R1_001_trimmed.fastq.gz' -p $n'_R2_001_trimmed.fastq.gz' $f $n'_R2_001.fastq.gz'
+    cutadapt -j 8 -g $primerF -G $primerR -o $n'_R1_001_trimmed.fastq.gz' -p $n'_R2_001_trimmed.fastq.gz' $f $n'_R2_001.fastq.gz'
 
 done
 
@@ -28,6 +31,10 @@ mkdir fastqc-R1
 fastqc *_R1_001_trimmed.fastq.gz -o fastqc-R1
 cd fastqc-R1
 multiqc --export .
+FASTQC_VAR1_50=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --input $fastq_directory/"fastqc-R1/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.50)
+FASTQC_VAR1_75=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --input $fastq_directory/"fastqc-R1/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.75)
+FASTQC_VAR1_85=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --input $fastq_directory/"fastqc-R1/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.85)
+FASTQC_VAR1_90=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py  --input $fastq_directory/"fastqc-R1/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.90)
 
 #read 2 fastqc, multiqc-------------------
 cd ..
@@ -35,54 +42,60 @@ mkdir fastqc-R2
 fastqc *_R2_001_trimmed.fastq.gz -o fastqc-R2
 cd fastqc-R2
 multiqc --export .
+FASTQC_VAR2_50=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --input $fastq_directory/"fastqc-R2/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.50)
+FASTQC_VAR2_75=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --input $fastq_directory/"fastqc-R2/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.75)
+FASTQC_VAR2_85=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --input $fastq_directory/"fastqc-R2/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.85)
+FASTQC_VAR2_90=$($scriptsPATH/fastqc_per_base_sequence_quality_dropoff.py --input $fastq_directory/"fastqc-R2/multiqc_data/mqc_fastqc_per_base_sequence_quality_plot_1.txt" --cutoff 0.90)
 
-#run helper R script to get quality scores-------------------
-fastqcPATH_R1=$fastq_directory/"fastqc-R1/multiqc_data"
-fastqcPATH_R2=$fastq_directory/"fastqc-R2/multiqc_data"
-
+#run dada2 filterandtrim-------------------
 cd $scriptsPATH
-Rscript --vanilla fastqc_per_base_quality_max.R ${fastq_directory} ${fastqcPATH_R1} ${fastqcPATH_R2}
-MAX_Q1=$(sed '2q;d' $fastq_directory/maxQ_R1.txt)
-MAX_Q2=$(sed '2q;d' $fastq_directory/maxQ_R2.txt)
-
-Q1_90=$(echo "$MAX_Q1 * 0.9" | bc)
-Q1_75=$(echo "$MAX_Q1 * 0.75" | bc)
-Q1_50=$(echo "$MAX_Q1 * 0.5" | bc)
-Q2_90=$(echo "$MAX_Q2 * 0.9" | bc)
-Q2_75=$(echo "$MAX_Q2 * 0.75" | bc)
-Q2_50=$(echo "$MAX_Q2 * 0.5" | bc)
-
-printf "\n"
-paste <(echo "cut-off") <(echo "") <(echo "read1") <(echo "read2")
-paste <(echo "") <(echo "max") <(printf %s "$MAX_Q1") <(printf %s "$MAX_Q2")
-paste <(echo "") <(echo "90%") <(printf %s "$Q1_90") <(printf %s "$Q2_90")
-paste <(echo "") <(echo "75%") <(printf %s "$Q1_75") <(printf %s "$Q2_75")
-paste <(echo "") <(echo "50%") <(printf %s "$Q1_50") <(printf %s "$Q2_50")
-printf "\n"
-
-#run dada2 with filter scores-------------------
-cd $scriptsPATH
-Rscript --vanilla dada2_runfile2.r ${fastq_directory} ${Q1_90} ${Q2_90}
-DADA2_VAR_90=$(sed '2q;d' $fastq_directory/average_quality.txt)
-rm $fastq_directory/filtered/*_filt.fastq.gz
-rmdir $fastq_directory/filtered
-
-Rscript --vanilla dada2_runfile2.r ${fastq_directory} ${Q1_75} ${Q2_75}
-DADA2_VAR_75=$(sed '2q;d' $fastq_directory/average_quality.txt)
-rm $fastq_directory/filtered/*_filt.fastq.gz
-rmdir $fastq_directory/filtered
-
-Rscript --vanilla dada2_runfile2.r ${fastq_directory} ${Q1_50} ${Q2_50}
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_50} ${FASTQC_VAR2_50}
 DADA2_VAR_50=$(sed '2q;d' $fastq_directory/average_quality.txt)
 rm $fastq_directory/filtered/*_filt.fastq.gz
 rmdir $fastq_directory/filtered
 
-#print values from dada2-------------------
+cd $scriptsPATH
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_75} ${FASTQC_VAR2_50}
+DADA2_VAR_75_50=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_75} ${FASTQC_VAR2_75}
+DADA2_VAR_75=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_85} ${FASTQC_VAR2_75}
+DADA2_VAR_85_75=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_85} ${FASTQC_VAR2_85}
+DADA2_VAR_85=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_90} ${FASTQC_VAR2_85}
+DADA2_VAR_90_85=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+Rscript --vanilla dada2_runfile.r ${fastq_directory} ${FASTQC_VAR1_90} ${FASTQC_VAR2_90}
+DADA2_VAR_90=$(sed '2q;d' $fastq_directory/average_quality.txt)
+rm $fastq_directory/filtered/*_filt.fastq.gz
+rmdir $fastq_directory/filtered
+
+
+#print values from multiqc-------------------
 printf "\n"
-paste <(echo "cut-off") <(echo "") <(echo "perc. retain")
-paste <(echo "") <(echo "90%")  <(printf %s "$DADA2_VAR_90")
-paste <(echo "") <(echo "75%")  <(printf %s "$DADA2_VAR_75")
-paste <(echo "") <(echo "50%")  <(printf %s "$DADA2_VAR_50")
+paste <(echo "cut-off") <(echo "") <(echo "read1") <(echo "read2") <(echo "perc. retain")
+paste <(echo "") <(echo "50%") <(printf %s "$FASTQC_VAR1_50") <(printf %s "$FASTQC_VAR2_50") <(printf %s "$DADA2_VAR_50")
+paste <(echo "") <(echo "50/75%") <(printf %s "$FASTQC_VAR1_75_50") <(printf %s "$FASTQC_VAR2_75") <(printf %s "$DADA2_VAR_50")
+paste <(echo "") <(echo "75%") <(printf %s "$FASTQC_VAR1_75") <(printf %s "$FASTQC_VAR2_75") <(printf %s "$DADA2_VAR_75")
+paste <(echo "") <(echo "75/85%") <(printf %s "$FASTQC_VAR1_85_75") <(printf %s "$FASTQC_VAR2_85") <(printf %s "$DADA2_VAR_75")
+paste <(echo "") <(echo "85%") <(printf %s "$FASTQC_VAR1_85") <(printf %s "$FASTQC_VAR2_85") <(printf %s "$DADA2_VAR_85")
+paste <(echo "") <(echo "85/90%") <(printf %s "$FASTQC_VAR1_85_90") <(printf %s "$FASTQC_VAR2_90") <(printf %s "$DADA2_VAR_85")
+paste <(echo "") <(echo "90%") <(printf %s "$FASTQC_VAR1_90") <(printf %s "$FASTQC_VAR2_90") <(printf %s "$DADA2_VAR_90")
 printf "\n"
 
 
